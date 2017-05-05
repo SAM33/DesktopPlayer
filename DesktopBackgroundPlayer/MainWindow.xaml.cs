77afddf2ed549c2e59e1ec9327195571b9c0c113
sam33 <sup3355@yahoo.com.tw>
2017-05-06 03:40:43 +0800

    second commit

---
+++ DesktopBackgroundPlayer/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Shapes;
+
+namespace DesktopBackgroundPlayer
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Player player;
+        bool isopen;
+        public MainWindow()
+        {
+            InitializeComponent();
+            player = new Player();
+            player.Hide();
+            player.SetAlpha(0.4);
+            isopen = false;
+            this.Closed += MainWindow_Closed;
+
+
+
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            player.Close();
+        }
+
+        private void button_pause_click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void button_play_click(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+        }
+
+        private void button_open_click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            String file = "";
+            if (openFileDialog.ShowDialog() == true)
+                file = openFileDialog.FileName;
+            if (!file.Equals(""))
+            {
+                try
+                {
+                    player.Show();
+                    player.Open(file);
+                    isopen = true;
+                    label.Content = "File : " + file;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Can't open " + file);
+                }
+            }
+        }
+
+        private void button_stop_click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+            player.Hide();
+            isopen = false;
+            label.Content = "File : ";
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (player != null)
+            {
+                player.SetAlpha(e.NewValue / 100);
+            }
+        }
+    }
+}
---
+++ DesktopBackgroundPlayer/Player.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+
+namespace DesktopBackgroundPlayer
+{
+    /// <summary>
+    /// Player.xaml 的互動邏輯
+    /// </summary>
+    /// 
+
+
+
+    public partial class Player : Window
+    {
+        private const int WS_EX_TRANSPARENT = 0x20;
+        private const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(IntPtr hwnd, int nIndex);
+
+        public Player()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            this.SourceInitialized += delegate
+            {
+                IntPtr hwnd = new WindowInteropHelper(this).Handle;
+                uint extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+                SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+            };
+            mediaElement.Opacity = 0.4;
+            this.Opacity = 0.4;
+            mediaElement.LoadedBehavior = MediaState.Manual;
+
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            mediaElement.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            mediaElement.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Left = 0;
+            this.Top = 0;
+            
+
+            mediaElement.MediaEnded += Media_Ended;
+
+        }
+
+        public void SetAlpha(double value)
+        {
+            this.Opacity = value;
+            mediaElement.Opacity = value;
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            mediaElement.Position = TimeSpan.Zero;
+            mediaElement.Play();
+        }
+
+        public void Open(String path)
+        {
+            mediaElement.Source = new Uri(path);
+            mediaElement.Pause();
+        }
+
+        public void Pause()
+        {
+            mediaElement.Pause();
+        }
+
+
+        public void Stop()
+        {
+            mediaElement.Stop();
+        }
+
+        public void Play()
+        {
+            mediaElement.Play();
+        }
+    }
+}
